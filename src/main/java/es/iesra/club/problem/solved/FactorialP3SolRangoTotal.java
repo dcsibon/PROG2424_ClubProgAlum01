60a51b0f5bab111e07351bd3ab6f999c3dd84b73
Diego Cano Sibón <dcsibon@gmail.com>
2025-01-16 15:08:46 +0100

    Update FactorialP3SolRangoTotal.java

--- src/main/java/es/iesra/club/problem/solved/FactorialP3SolRangoTotal.java
+++ src/main/java/es/iesra/club/problem/solved/FactorialP3SolRangoTotal.java
@@ -15,44 +15,11 @@ public class FactorialP3SolRangoTotal extends ProblemSolver {
     protected List<String> process(List<String> inputLines) {
         List<String> outputLines = new ArrayList<>();
 
-        long total = 0;
+        
 
-        for (String line : inputLines) {
-            int n = Integer.parseInt(line.trim());
-
-            if (n >= 0 && n <= 25) {
-                StringBuilder detail = new StringBuilder(n + "! -> ");
-                long factorial = calculateFactorialRecursive(n, detail);
-                detail.append(" = ").append(factorial);
-                total += factorial;
-
-                outputLines.add(detail.toString());
-            } else {
-                outputLines.add(n + "! -> fuera de rango");
-            }
-        }
-
-        outputLines.add("total = " + total);
         return outputLines;
     }
 
-
-    /**
-     * Calcula el factorial de un número utilizando recursividad para n > 1.
-     *
-     * @param n Número del que se calcula el factorial.
-     * @param detail Cadena que acumula los detalles de la operación.
-     * @return Factorial del número.
-     */
-    private long calculateFactorialRecursive(int n, StringBuilder detail) {
-        if (n <= 1) {
-            detail.append("1");
-            return 1;
-        } else {
-            detail.append(n).append("x");
-            return n * calculateFactorialRecursive(n - 1, detail);
-        }
-    }
 
 
     public static void main(String[] args) {
