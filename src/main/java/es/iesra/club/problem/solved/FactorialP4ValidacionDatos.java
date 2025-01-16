75c6666dd7d16c28377d0260a13cfbde57d8dd25
Diego Cano Sibón <dcsibon@gmail.com>
2025-01-16 15:08:21 +0100

    Update FactorialP4ValidacionDatos.java

--- src/main/java/es/iesra/club/problem/solved/FactorialP4ValidacionDatos.java
+++ src/main/java/es/iesra/club/problem/solved/FactorialP4ValidacionDatos.java
@@ -15,57 +15,11 @@ public class FactorialP4ValidacionDatos extends ProblemSolver {
     protected List<String> process(List<String> inputLines) {
         List<String> outputLines = new ArrayList<>();
 
-        long total = 0;
 
-        for (String line : inputLines) {
-            String trimmedLine = line.trim();
-            boolean process = false; // Indica si el número es válido para procesamiento
-            int n = 0;
-
-            // Validar si la línea puede convertirse a entero
-            try {
-                n = Integer.parseInt(trimmedLine);
-                process = true;
-            } catch (NumberFormatException e) {
-                // Si no es un número, registrar un error
-                outputLines.add(trimmedLine + " -> error");
-            }
-
-            // Procesar números válidos
-            if (process) {
-                if (n >= 0 && n <= 25) {
-                    StringBuilder detail = new StringBuilder(n + "! -> ");
-                    long factorial = calculateFactorialRecursive(n, detail);
-                    detail.append(" = ").append(factorial);
-                    total += factorial;
-
-                    outputLines.add(detail.toString());
-                } else {
-                    outputLines.add(n + "! -> fuera de rango");
-                }
-            }
-        }
-
-        outputLines.add("total = " + total);
+        
         return outputLines;
     }
 
-    /**
-     * Calcula el factorial de un número utilizando recursividad para n > 1.
-     *
-     * @param n Número del que se calcula el factorial.
-     * @param detail Cadena que acumula los detalles de la operación.
-     * @return Factorial del número.
-     */
-    private long calculateFactorialRecursive(int n, StringBuilder detail) {
-        if (n == 1) {
-            detail.append("1");
-            return 1;
-        } else {
-            detail.append(n).append("x");
-            return n * calculateFactorialRecursive(n - 1, detail);
-        }
-    }
 
 
     public static void main(String[] args) {
